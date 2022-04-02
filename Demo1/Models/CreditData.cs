22e3e40db51aebfb5d7c2205d536e6cfeea8a98d
Tony Frazier <afrazier@traumazone.com>
2022-04-02 17:52:35 -0400

    Add data models

---
+++ Demo1/Models/CreditData.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo1.Models
+{
+    public class CreditData
+    {
+        public long ApplicationId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string Bureau { get; set; } = string.Empty;
+        public decimal MinPaymentPercentage { get; set; }
+        public IList<Tradeline> Tradelines { get; set; } = new List<Tradeline>();
+    }
+}
---
+++ Demo1/Models/Tradeline.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo1.Models
+{
+    public class Tradeline
+    {
+        public long TradelineId { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public decimal MonthlyPayment { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public bool IsMortgage { get; set; }
+    }
+}
