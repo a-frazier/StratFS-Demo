4e566d14bffdd2ec6e9a6bae3780a500296eab0e
Tony Frazier <afrazier@traumazone.com>
2022-04-02 20:55:04 -0400

    Add CreditDataService

---
+++ Demo1/Services/CreditDataService.cs
@@ -0,0 +1,33 @@
+﻿using Demo1.Models;
+
+namespace Demo1.Services
+{
+    internal class CreditDataService
+    {
+        private IHttpClientFactory _httpClientFactory;
+        private const string dataSourceUrl = "https://raw.githubusercontent.com/StrategicFS/Recruitment/master/creditData.json";
+
+        public CreditDataService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            CreditData = new Lazy<IList<CreditDatum>>(FetchCreditData, LazyThreadSafetyMode.PublicationOnly);
+        }
+
+        private IList<CreditDatum> FetchCreditData()
+        {
+            using var client = _httpClientFactory.CreateClient();
+
+            // N.B.  I know this is not ideal, but I'm trying to prevent the Lazy property from being "infected" by async.
+            var data = client.GetFromJsonAsync<ResponseContainer>(dataSourceUrl).Result;
+
+            return data?.CreditReports ?? new List<CreditDatum>();
+        }
+
+        public Lazy<IList<CreditDatum>> CreditData { get; }
+
+        private class ResponseContainer
+        {
+            public IList<CreditDatum> CreditReports { get; set; } = new List<CreditDatum>();
+        }
+    }
+}
